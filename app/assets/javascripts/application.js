e23370dc480809708a39c7f0e1f56cc15e9e796f
gui_desan <gui_desan@outlook.com>
2019-06-11 14:49:27 +0700

    Fix bootstrap import

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,9 @@
 // about supported directives.
 //
 //= require activestorage
-//= require bootstrap-sprockets
-//= require jquery3
-//= require popper
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
