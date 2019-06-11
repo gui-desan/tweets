399e3d26d44af3b090d9af3df70e422c9fe2ee92
gui_desan <gui_desan@outlook.com>
2019-06-11 17:16:44 +0700

    Change migration order

--- db/migrate/20190611064640_create_tweets.rb
+++ db/migrate/20190611064640_create_tweets.rb
@@ -1,7 +1,9 @@
 class CreateTweets < ActiveRecord::Migration[5.2]
   def change
     create_table :tweets do |t|
+      t.references :user, foreign_key: true
       t.string :content, limit: 256
+
       t.timestamps
     end
   end
